b1f551f7ceaf94789594a6dd5cb752bdc95780cc
Xiaozuo <369301826@qq.com>
2018-07-18 11:19:21 +0800

    基本框架完成

--- 文件压缩项目/FileCompress.h
+++ 文件压缩项目/FileCompress.h
@@ -1,3 +1,51 @@
 #pragma once
 
 
+//########################################################
+//###################   头文件  ##########################
+//########################################################
+
+#include <string>
+using namespace std;
+#include "HuffmanTree.h"
+
+//########################################################
+//#################   文件压缩类  ########################
+//########################################################
+
+//结点信息，存放字符，出现此时，编码
+struct CharData {
+	unsigned char _ch;
+	size_t _count;
+	string code;
+};
+
+
+class FileCompress {
+	typedef struct HuffmanTreeNode<CharData> Node;
+public:
+	FileCompress();//缺省构造函数
+	void Compress();//压缩
+	void UnCompress();//解压
+	void CreateHuffmanCode();//生成Huffman编码
+
+private:
+	CharData _hashtable[256];
+};
+
+FileCompress::FileCompress()//缺省构造函数
+{
+
+}
+void FileCompress::Compress()//压缩
+{
+
+}
+void FileCompress::UnCompress()//解压
+{
+
+}
+void FileCompress::CreateHuffmanCode()//生成Huffman编码
+{
+
+}
--- 文件压缩项目/HuffmanTree.h
+++ 文件压缩项目/HuffmanTree.h
@@ -3,6 +3,29 @@
 template<class T>
 struct HuffmanTreeNode{
 	T _data;
+	HuffmanTreeNode<T>* _left;
+	HuffmanTreeNode<T>* _right;
+	HuffmanTreeNode<T>* _parent;
 
+	HuffmanTreeNode<T>()
+		:_left(NULL)
+		:_right(NULL)
+		:_parent(NULL)
+	{
+		;
+	}
+};
 
+template<class T>
+class HuffmanTree {
+	typedef struct HuffmanTreeNode<T> Node;
+public:
+	HuffmanTree()
+		_root(NULL)
+	{}
+	~HuffmanTree()
+	{}
+	void insert();
+private:
+	Node* _root;
 };
